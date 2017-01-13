1c4a3069ab7d2acefe38f9c9d03e5e5dc2fa27c7
eidietrich <eric.dietrich@gmail.com>
2017-01-12 19:26:42 -0700

    Further development

--- NOTES.md
+++ NOTES.md
@@ -19,10 +19,9 @@ County-specific numbers available in quarterly census of employment and wages (Q
 ## TODO
 - Download/scrape MIT Living wage estimates for particular county - set up a script to do this
 - Decide: Calculator versus Stepped/scrolled story (start by building calculator, find a story, then set up narrative --> interactive)
-- For table responsiveness - cut down on number of columns, flip worker number into occupation tab
-- Untangle coordination between CostContainer and WageContainer so living wage updates flow downward in calculator
 - Change dropdown menu on profession selector to a link-based system
-- 
+- Add toggle between annual and hourly rates?
+- Changes (after showing it to Troy) --> Retitle protective services, healthcare support. Spell out MIT. Figure out how to explain occupation labels more.
 
 
 ## Outline:
--- src/App.js
+++ src/App.js
@@ -5,28 +5,36 @@ import './App.css';
 import WageContainer from './WageContainer.js';
 import CostContainer from './CostContainer.js';
 
-import wageData from './../data/sw-mt-wages.json';
-import costData from './../data/mit-living-expenses.json'
+import wageData from './../data/sw-mt-wages-hand-cleaned.json';
+import costData from './../data/mit-living-expenses-hand-cleaned.json'
 
-// let familySize = "1 Adult";
+// let familySize = "1 Adult";
 // let costData = mitLiving[familySize];
 
-const familySizes = ["1 Adult", "1 Adult 1 Child", "1 Adult 2 Children", "1 Adult 3 Children", "2 Adults (1 Working)", "2 Adults (1 Working) 1 Child", "2 Adults (1 Working) 2 Children", "2 Adults (1 Working) 3 Children", "2 Adults", "2 Adults 1 Child", "2 Adults 2 Children", "2 Adults 3 Children"];
+const familySizes = [
+  "1 Adult", "1 Adult 1 Child", "1 Adult 2 Children", "1 Adult 3 Children",
+  "2 Adults (2 Working)", "2 Adults (2 Working) 1 Child", "2 Adults (2 Working) 2 Children", "2 Adults (2 Working) 3 Children",
+  "2 Adults (1 Working)", "2 Adults (1 Working) 1 Child", "2 Adults (1 Working) 2 Children", "2 Adults (1 Working) 3 Children",
+];
+
+const DUAL_INCOMES =  ["2 Adults (2 Working)", "2 Adults  (2 Working) 1 Child", "2 Adults (2 Working) 2 Children", "2 Adults (2 Working) 3 Children"] // Hacky -- TODO: Consolidate this sort of data
 
 var App = React.createClass({
   getInitialState: function(){
     return {
-      focusFamilySize: familySizes[0],
-      livingWage: 21570, // Highlight salary cells green if above this value, red if below it
+      focusFamilySize: familySizes[6],
+      numIncomes: 2,
+      livingWage: 32684,
     }
   },
-  setFocusFamilySize: function(e){
-    console.log(e.target.value);
-    let newFocusFamilySize = e.target.value;
-    let newLivingWage = costData[newFocusFamilySize]['Required post-tax income'];
+  setFocusFamilySize: function(newFocusFamilySize){
+    console.log(newFocusFamilySize);
+    let newLivingWage = costData[newFocusFamilySize]['Total'];
+    let newNumIncomes = (DUAL_INCOMES.indexOf(newFocusFamilySize) >= 0) ? 2 : 1;
     this.setState({
       focusFamilySize: newFocusFamilySize,
-      livingWage: newLivingWage
+      livingWage: newLivingWage / newNumIncomes,
+      numIncomes: newNumIncomes,
     });
   },
   render: function() {
@@ -37,8 +45,8 @@ var App = React.createClass({
           data={costData}
           familySizes={familySizes}
           focusFamilySize={this.state.focusFamilySize}
-          setFocusFamilySize={this.setFocusFamilySize}
-          updateLivingWage={this.setLivingWage}/>
+          numIncomes={this.state.numIncomes}
+          setFocusFamilySize={this.setFocusFamilySize} />
         <hr />
         <h2>And who actually makes it?</h2>
         <WageContainer
